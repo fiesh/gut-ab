e96906b5415339769482530a9b4f84134ea1f91d
Kahrl <kahrl@mailinator.com>
2011-03-13 04:17:12 +0100

    FragmentLoader.php, WikiLoader.php: support loading fragments from Guttenberg-2006/, support loading list of fragment types

--- FragmentLoader.php
+++ FragmentLoader.php
@@ -32,10 +32,8 @@ class FragmentLoader {
 		return $cats;
 	}
 
-	static public function getFragments()
+	static private function processFrags($entries, $titleBlacklist=array())
 	{
-		$titleBlacklist = array('Fragment 99999 11-22');
-		$entries = WikiLoader::getEntriesWithPrefix('Fragment', true, true);
 		$fragments = array();
 		foreach($entries as $e) {
 			$a = self::processString($e['revisions'][0]['*']);
@@ -45,5 +43,47 @@ class FragmentLoader {
 				$fragments[] = $a;
 		}
 		return $fragments;
+	}
+
+	static public function getFragments()
+	{
+		$entries = WikiLoader::getEntriesWithPrefix('Fragment', true, true);
+		$titleBlacklist = array('Fragment 99999 11-22');
+		return self::processFrags($entries, $titleBlacklist);
+	}
+
+	static public function getFragmentsG2006()
+	{
+		$entries = WikiLoader::getEntriesWithPrefix('Guttenberg-2006/', true, true);
+		$fragmentTitles = array();
+		foreach($entries as $e) {
+			if(preg_match('/^Guttenberg-2006\/(\d{3})$/', $e['title'], $match) && $match[1] >= 1) {
+				$pagenum = (int) $match[1];
+				$raw = $e['revisions'][0]['*'];
+				$raw = preg_replace('/<!--.*?-->/s', '', $raw);
+				preg_match_all('/\{\{:([- _A-Za-z0-9\/]*?)\}\}/', $raw, $match);
+				$i = 0;
+				while(isset($match[1][$i])) {
+					$fragmentTitles[] = str_replace('_', ' ', $match[1][$i]);
+					++$i;
+				}
+			}
+		}
+
+		$fragmentEntries = WikiLoader::getEntriesByTitles($fragmentTitles);
+		return self::processFrags($fragmentEntries);
+	}
+
+	static public function getFragmentTypes()
+	{
+		$pageids = WikiLoader::getCategoryMembers('Kategorie:PlagiatsKategorien');
+		$entries = WikiLoader::getEntries($pageids, true, true);
+
+		$fragtypes = array();
+		foreach($entries as $entry) {
+			if (substr($entry['title'], 0, 10) == 'Kategorie:')
+				$fragtypes[] = $entry['title'];
+		}
+		return $fragtypes;
 	}
 }
